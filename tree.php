a819c930d8a19e2fe5a4b2c9a54d0023068bdadb
IqraHabib <44527726+IqraHabib@users.noreply.github.com>
2018-12-26 21:24:17 +0500

    tress
    Importance of trees using php

---
+++ tree.php
@@ -0,0 +1,17 @@
+<p><h1><em><u>Importance of TREES</u></em></p></h1>
+<img scr="C:\xampp\htdocs\wad\treepic.jpg">
+<h3>
+<?php
+$a= array("Trees gives us oxygen that is very esential for humans.",
+"Trees provide shade and shelter, timber for construction, fuel for cooking and heating, and fruit for food.",
+"Trees and forests provide a habitat for many species of animals and plants.", 
+"Trees help to control pollution as they absorb carbon dioxide, thus improving our environment.",
+"Trees cause rainfall and protect water resources under the ground. They prevent floods and droughts.");
+
+for($i=0; $i< 5; $i++)
+{
+    echo  "<li>".$a[$i]."</li>";
+}
+?>
+
+</h3>
